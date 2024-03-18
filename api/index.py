ff8072b28843fde6ff715fe23169fb2f1e8d168c
MintLatte77 <74120838+MintLatte77@users.noreply.github.com>
2024-03-18 21:16:47 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,14 +16,14 @@ def meal():
     body = request.get_json()
     print(body)
     print(body['userRequest']['timezone'])
-
+    timezone = body['userRequest']['timezone']
     responseBody = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
                     "simpleText": {
-                        "text": "테스트"
+                        "text": timezone
                     }
                 }
             ]
