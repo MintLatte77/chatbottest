06d9e7bb57f7b4bb437928e167114a51f1cafe3a
MintLatte77 <74120838+MintLatte77@users.noreply.github.com>
2024-03-19 17:33:47 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,6 +12,7 @@ timezone_kst = timezone(timedelta(hours=9))
 datetime_kst = datetime_utc.astimezone(timezone_kst)
 day = datetime_kst.strftime("%Y%m%d")
 date = str(int(datetime_kst.strftime("%m"))) + "월 "+ str(int(datetime_kst.strftime("%d"))) + "일"
+week = int(datetime_kst.strftime("%w"))
 
 # NEIS 설정
 url = "https://open.neis.go.kr/hub/mealServiceDietInfo"
@@ -64,14 +65,22 @@ def service():
 		'ATPT_OFCDC_SC_CODE' : edu_code,
 		'SD_SCHUL_CODE' : school_code,
 		'MLSV_YMD' : day
-	}
+		}
 
 	response = requests.get(url, params=params)
 	contents = response.text
-	findstart = contents.find('DDISH_NM') + 11
-	findend = contents.find('ORPLC_INFO') - 3
-	content = contents[findstart:findend]
-	meal ="\n".join(content.split('<br/>'))
+
+	#급식 미제공 날짜 구별
+	find = contents.find('해당하는 데이터가 없습니다.')
+	
+	if find == -1:
+		meal = "오늘은 급식이 없습니다!
+	else:
+		findstart = contents.find('DDISH_NM') + 11
+		findend = contents.find('ORPLC_INFO') - 3
+		content = contents[findstart:findend]
+		meal ="\n".join(content.split('<br/>'))
+	
 	responseBody = {
         "version": "2.0",
         "template": {
