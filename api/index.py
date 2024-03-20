52038e3209e79dcb14d3bffc0006b53ffe5b7ae7
MintLatte77 <74120838+MintLatte77@users.noreply.github.com>
2024-03-20 18:59:35 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -37,7 +37,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+	return 'Hello, World!'
 
 @app.route('/time')
 def time():
@@ -54,22 +54,22 @@ def agree1():
 	responesebody = {
   "version": "2.0",
   "template": {
-    "outputs": [
-      {
-        "textCard": {
-          "title": "개인정보 수집 및 이용 동의서",
-          "description": "'오늘 급식 뭐임'(이하 '서비스')은 급식, 시간표, 학사 일정 확인 등을 위해 「개인정보 보호법」 제 15조 1항에 따라 보유 기간(25년 2월 28일)까지 사용자의 개인정보(사용자 ID, 학교, 학년, 반)를 수집 및 이용합니다. 사용자는 서비스 이용에 필요한 최소한의 개인정보 수집 및 이용에 동의하지 않을 수 있으나, 동의를 거부 할 경우 서비스 이용이 불가합니다.\n아래 동의 버튼 클릭 시 해당 내용에 동의한 것으로 간주됩니다.",
-        "buttons": [
-            {
-              "action": "block",
-              "label": "동의",
-              "blockId" : "65faa5ef091618695a7fee9c"
-            }
+	"outputs": [
+	  {
+		"textCard": {
+		  "title": "개인정보 수집 및 이용 동의서",
+		  "description": "\'오늘 급식 뭐임\'(이하 \'서비스\')은 급식, 시간표, 학사 일정 확인 등을 위해 「개인정보 보호법」 제 15조 1항에 따라 보유 기간(25년 2월 28일)까지 사용자의 개인정보(사용자 ID, 학교, 학년, 반)를 수집 및 이용합니다. 사용자는 서비스 이용에 필요한 최소한의 개인정보 수집 및 이용에 동의하지 않을 수 있으나, 동의를 거부 할 경우 서비스 이용이 불가합니다.\n아래 동의 버튼 클릭 시 해당 내용에 동의한 것으로 간주됩니다.",
+		"buttons": [
+			{
+			  "action": "block",
+			  "label": "동의",
+			  "blockId" : "65faa5ef091618695a7fee9c"
+			}
 		]
 		}
 		  
-      }
-    ]
+	  }
+	]
   }
 }
 	return responesebody
@@ -79,30 +79,30 @@ def agree2():
 	responesebody = {
   "version": "2.0",
   "template": {
-    "outputs": [
-      {
-        "textCard": {
-          "title": "학교를 입력해 주세요",
-          "description": "현재 삼남중학교, 언양고등학교만 지원합니다. 학교 추가를 바라신다면 상담직원 연결을 눌러주세요."
+	"outputs": [
+	  {
+		"textCard": {
+		  "title": "학교를 입력해 주세요",
+		  "description": "현재 삼남중학교, 언양고등학교만 지원합니다. 학교 추가를 바라신다면 상담직원 연결을 눌러주세요."
 		} 
-      }
-    ],
-    "quickReplies": [
-      {
-        "messageText": "삼남중학교",
-        "action": "block",
-        "label": "삼남중학교"
-		"blockId": "65faa603e8b2137164330ae3",
-		"extra" : "S"
-      },
-      {
-        "messageText": "언양고등학교",
-        "action": "block",
-        "label": "언양고등학교"
-		"blockId": "65faa603e8b2137164330ae3",
-		"extra" : "E"
-      }
-    ]
+	  }
+	],
+	"quickReplies": [
+	  {
+		"messageText": "삼남중학교",
+		"action": "block",
+		"label": "삼남중학교",
+	"blockId": "65faa603e8b2137164330ae3",
+	"extra" : "S"
+	  },
+	  {
+		"messageText": "언양고등학교",
+		"action": "block",
+		"label": "언양고등학교",
+	"blockId": "65faa603e8b2137164330ae3",
+	"extra" : "E"
+	  }
+	]
   }
 }
 	return responesebody
@@ -115,36 +115,36 @@ def school():
 	responesebody = {
   "version": "2.0",
   "template": {
-    "outputs": [
-      {
-        "textCard": {
-          "title": "학년을 입력해 주세요"
+	"outputs": [
+	  {
+		"textCard": {
+		  "title": "학년을 입력해 주세요"
 		} 
-      }
-    ],
-    "quickReplies": [
-      {
-        "messageText": "1학년",
-        "action": "block",
-        "label": "1학년"
-		"blockId": "65faa61da0a1dd2d9e02e80e",
-		"extra" : userschool + "1"
-      },
-      {
-        "messageText": "2학년",
-        "action": "block",
-        "label": "2학년"
+	  }
+	],
+	"quickReplies": [
+	  {
+		"messageText": "1학년",
+		"action": "block",
+		"label": "1학년",
+	"blockId": "65faa61da0a1dd2d9e02e80e",
+	"extra" : userschool + "1"
+	  },
+	  {
+		"messageText": "2학년",
+		"action": "block",
+		"label": "2학년",
 		"blockId": "65faa61da0a1dd2d9e02e80e",
 		"extra" : userschool + "2"
-      },
+	  },
 	{
-        "messageText": "3학년",
-        "action": "block",
-        "label": "3학년"
+		"messageText": "3학년",
+		"action": "block",
+		"label": "3학년",
 		"blockId": "65faa61da0a1dd2d9e02e80e",
 		"extra" : userschool + "3"
-      }
-    ]
+	  }
+	]
   }
 }
 	return responesebody
@@ -157,79 +157,79 @@ def grade():
 	responesebody = {
   "version": "2.0",
   "template": {
-    "outputs": [
-      {
-        "textCard": {
-          "title": "반을 입력해 주세요"
+	"outputs": [
+	  {
+		"textCard": {
+		  "title": "반을 입력해 주세요"
 		} 
-      }
-    ],
-    "quickReplies": [
-      {
-        "messageText": "1반",
-        "action": "block",
-        "label": "1반"
+	  }
+	],
+	"quickReplies": [
+	  {
+		"messageText": "1반",
+		"action": "block",
+		"label": "1반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "1"
-      },
+	  },
 	{
-        "messageText": "2반",
-        "action": "block",
-        "label": "2반"
+		"messageText": "2반",
+		"action": "block",
+		"label": "2반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "2"
-      },
+	  },
 	{
-        "messageText": "3반",
-        "action": "block",
-        "label": "3반"
+		"messageText": "3반",
+		"action": "block",
+		"label": "3반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "3"
-      },
+	  },
 	{
-        "messageText": "4반",
-        "action": "block",
-        "label": "4반"
+		"messageText": "4반",
+		"action": "block",
+		"label": "4반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "4"
-      },
+	  },
 	{
-        "messageText": "5반",
-        "action": "block",
-        "label": "5반"
+		"messageText": "5반",
+		"action": "block",
+		"label": "5반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "5"
-      },
+	  },
 	{
-        "messageText": "6반",
-        "action": "block",
-        "label": "6반"
+		"messageText": "6반",
+		"action": "block",
+		"label": "6반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "6"
-      },
+	  },
 	{
-        "messageText": "7반",
-        "action": "block",
-        "label": "7반"
+		"messageText": "7반",
+		"action": "block",
+		"label": "7반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "7"
-      },
+	  },
 	{
-        "messageText": "8반",
-        "action": "block",
-        "label": "8반"
+		"messageText": "8반",
+		"action": "block",
+		"label": "8반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "8"
-      },
+	  },
 	{
-        "messageText": "9반",
-        "action": "block",
-        "label": "9반"
+		"messageText": "9반",
+		"action": "block",
+		"label": "9반",
 		"blockId": "65faa630d7cbb10c92facd52",
 		"extra" : usergrade + "9"
-      }
-      
-    ]
+	  }
+	  
+	]
   }
 }
 	return responesebody
@@ -253,27 +253,27 @@ def class1():
 	responesebody = {
   "version": "2.0",
   "template": {
-    "outputs": [
-      {
-        "textCard": {
-          "title": "입력한 정보가 맞는지 확인해 주세요",
-          "description": description,
-        "buttons": [
-            {
-              "action": "block",
-              "label": "맞아요!",
-              "blockId" : "65faaca1a64303558477aa63"
-            },
+	"outputs": [
+	  {
+		"textCard": {
+		  "title": "입력한 정보가 맞는지 확인해 주세요",
+		  "description": description,
+		"buttons": [
 			{
-              "action": "block",
-              "label": "아니에요",
-              "blockId" : "65faa5ef091618695a7fee9c"
-            }
+			  "action": "block",
+			  "label": "맞아요!",
+			  "blockId" : "65faaca1a64303558477aa63"
+			},
+			{
+			  "action": "block",
+			  "label": "아니에요",
+			  "blockId" : "65faa5ef091618695a7fee9c"
+			}
 		]
 		}
 		  
-      }
-    ]
+	  }
+	]
   }
 }
 	return responesebody
@@ -285,29 +285,29 @@ def check():
 	userinfo = body['action']['clientExtra']
 	sign = str(userID) + "," + str(userinfo) + ","
 	with open('data.txt', 'a') as data:
-    	data.write(sign)
+		data.write(sign)
 	
 	responesebody = {
   "version": "2.0",
   "template": {
-    "outputs": [
-      {
-        "textCard": {
-          "title": "입력한 정보가 등록되었어요",
-        "buttons": [
-            {
-              "action": "message",
-              "label": "오늘 급식 뭐임?"
-            },
+	"outputs": [
+	  {
+		"textCard": {
+		  "title": "입력한 정보가 등록되었어요",
+		"buttons": [
 			{
-              "action": "message",
-              "label": "오늘 시간표 뭐임?"
-            }
+			  "action": "message",
+			  "label": "오늘 급식 뭐임?"
+			},
+			{
+			  "action": "message",
+			  "label": "오늘 시간표 뭐임?"
+			}
 		]
 		}
 		  
-      }
-    ]
+	  }
+	]
   }
 }
 	return responesebody
@@ -325,47 +325,47 @@ def timetable():
 	responseBody = {
   "version": "2.0",
   "template": {
-    "outputs": [
-      {
-        "carousel": {
-          "type": "itemCard",
-          "items": [
-            {
-              "imageTitle": {
-                "title": date + " 시간표" 
-              },
-              "itemList": [
-                {
-                  "title": '1교시',
-                  "description": timetablelist[1] + ' | ' + teacherlist[1]
-                 },
+	"outputs": [
+	  {
+		"carousel": {
+		  "type": "itemCard",
+		  "items": [
+			{
+			  "imageTitle": {
+				"title": date + " 시간표" 
+			  },
+			  "itemList": [
+				{
+				  "title": '1교시',
+				  "description": timetablelist[1] + ' | ' + teacherlist[1]
+				 },
 		{
-                  "title": '2교시',
-                  "description": timetablelist[2] + ' | ' + teacherlist[2]
-                 },
+				  "title": '2교시',
+				  "description": timetablelist[2] + ' | ' + teacherlist[2]
+				 },
 		{
-                  "title": '3교시',
-                  "description": timetablelist[3] + ' | ' + teacherlist[3]
-                 },
+				  "title": '3교시',
+				  "description": timetablelist[3] + ' | ' + teacherlist[3]
+				 },
 		{
-                  "title": '4교시',
-                  "description": timetablelist[4] + ' | ' + teacherlist[4]
-                 },
+				  "title": '4교시',
+				  "description": timetablelist[4] + ' | ' + teacherlist[4]
+				 },
 		{
-                  "title": '5교시',
-                  "description": timetablelist[5] + ' | ' + teacherlist[5]
-                 },
+				  "title": '5교시',
+				  "description": timetablelist[5] + ' | ' + teacherlist[5]
+				 },
 		{
-                  "title": '6교시',
-                  "description": timetablelist[6] + ' | ' + teacherlist[6]
-                 }
-              ],
-              "itemListAlignment": "left"
-	    }
+				  "title": '6교시',
+				  "description": timetablelist[6] + ' | ' + teacherlist[6]
+				 }
+			  ],
+			  "itemListAlignment": "left"
+		}
 	  ]
 	}
-      }
-    ]
+	  }
+	]
   }
 	}
 	return responseBody
@@ -400,32 +400,32 @@ def service():
 	# 시간표
 	if week == 0 or week == 6:
 		responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-        			"textCard": {
-          				"title": date + " 오늘의 급식",
-          				"description": meal ,
-          				"buttons": [
-            				{
-              					"action": "webLink",
-              					"label": "온라인 건의함",
-              					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
-            				}
-            						]
-        						},
+		"version": "2.0",
+		"template": {
+			"outputs": [
+				{
 					"textCard": {
-          				"title": date + " 시간표",
-          				"description": "오늘은 시간표가 없어요" ,
-          				"buttons": [
-            				{
-              					"action": "webLink",
-              					"label": "온라인 건의함",
-              					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
-            				}
-            						]
-        						}
+		  				"title": date + " 오늘의 급식",
+		  				"description": meal ,
+		  				"buttons": [
+							{
+			  					"action": "webLink",
+			  					"label": "온라인 건의함",
+			  					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
+							}
+									]
+								},
+					"textCard": {
+		  				"title": date + " 시간표",
+		  				"description": "오늘은 시간표가 없어요" ,
+		  				"buttons": [
+							{
+			  					"action": "webLink",
+			  					"label": "온라인 건의함",
+			  					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
+							}
+									]
+								}
 				}
 						]
 		}
@@ -443,77 +443,77 @@ def service():
 
 	if week == 3:
 		responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-        			"textCard": {
-          				"title": date + " 오늘의 급식",
-          				"description": meal ,
-          				"buttons": [
-            				{
-              					"action": "webLink",
-              					"label": "온라인 건의함",
-              					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
-            				}
-            						]
-        						}
+		"version": "2.0",
+		"template": {
+			"outputs": [
+				{
+					"textCard": {
+		  				"title": date + " 오늘의 급식",
+		  				"description": meal ,
+		  				"buttons": [
+							{
+			  					"action": "webLink",
+			  					"label": "온라인 건의함",
+			  					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
+							}
+									]
+								}
 				}
 						]
 		}
 	}
 	else:
 		responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-        			"textCard": {
-          				"title": date + " 오늘의 급식",
-          				"description": meal ,
-          				"buttons": [
-            				{
-              					"action": "webLink",
-              					"label": "온라인 건의함",
-              					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
-            				}
-            						]
-        						},
+		"version": "2.0",
+		"template": {
+			"outputs": [
+				{
+					"textCard": {
+		  				"title": date + " 오늘의 급식",
+		  				"description": meal ,
+		  				"buttons": [
+							{
+			  					"action": "webLink",
+			  					"label": "온라인 건의함",
+			  					"webLinkUrl": "https://m.site.naver.com/1k4Sj"
+							}
+									]
+								},
 					"listCard": {
-         		 		"header": {
-            				"title": date + " 시간표"
-          						  },
-          				"items": [
-            				{
-              					"title": timetablelist[0],
-              					"description": teacherlist[0]
-            				},
+		 		 		"header": {
+							"title": date + " 시간표"
+		  						  },
+		  				"items": [
 							{
-              					"title": timetablelist[1],
-              					"description": teacherlist[1]
-            				},
+			  					"title": timetablelist[0],
+			  					"description": teacherlist[0]
+							},
 							{
-              					"title": timetablelist[2],
-              					"description": teacherlist[2]
-            				},
+			  					"title": timetablelist[1],
+			  					"description": teacherlist[1]
+							},
 							{
-              					"title": timetablelist[3],
-              					"description": teacherlist[3]
-            				},
+			  					"title": timetablelist[2],
+			  					"description": teacherlist[2]
+							},
 							{
-              					"title": timetablelist[4],
-              					"description": teacherlist[4]
-            				},
+			  					"title": timetablelist[3],
+			  					"description": teacherlist[3]
+							},
 							{
-              					"title": timetablelist[5],
-              					"description": teacherlist[5]
-            				},
+			  					"title": timetablelist[4],
+			  					"description": teacherlist[4]
+							},
 							{
-              					"title": timetablelist[6],
-              					"description": teacherlist[6]
-            				}
+			  					"title": timetablelist[5],
+			  					"description": teacherlist[5]
+							},
+							{
+			  					"title": timetablelist[6],
+			  					"description": teacherlist[6]
+							}
 								  ]
-        						}
+								}
 				}
 						]
 		}
